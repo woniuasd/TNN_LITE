380c65e4276cee26c0254730c75460b10b3158fb
zhangweikang <kk@18810532057@gmail.com>
2022-07-03 18:48:47 +0800

    删除无关代码

--- benchmark/benchmark_android/benchmark_app/benchmark/src/main/java/com/tencent/tnn/benchmark/BenchmarkModel.java
+++
@@ -1,5 +0,0 @@
-package com.tencent.tnn.benchmark;
-
-public class BenchmarkModel {
-    public native int nativeRun(String args, String fileDir);
-}
---
+++ examples/linux/x86/run_x86_imageclassify.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./demo_x86_imageclassify -p ../../../../model/SqueezeNet/squeezenet_v1.1.tnnproto -m ../../../../model/SqueezeNet/squeezenet_v1.1.tnnmodel -i ../../../assets/tiger_cat.jpg
+
