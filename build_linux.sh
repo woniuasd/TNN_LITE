95e639ea7439f043df529b09d65134a4a753a00a
zhangweikang <kk@18810532057@gmail.com>
2023-04-17 21:16:51 +0800

    优化example代码结构,添加编译脚本

---
+++ build_linux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SHARED_LIB="ON"
+OPENMP="ON"
+OPENCL="OFF"
+QUANTIZATION="OFF"
+CC=gcc
+CXX=g++
+
+mkdir build
+cd build
+
+cmake .. \
+    -DCMAKE_SYSTEM_NAME=Linux  \
+    -DTNN_TEST_ENABLE=ON \
+    -DTNN_CPU_ENABLE=ON \
+    -DCMAKE_C_COMPILER=$CC \
+    -DCMAKE_CXX_COMPILER=$CXX \
+    -DTNN_X86_ENABLE:BOOL=ON \
+    -DTNN_OPENMP_ENABLE:BOOL=$OPENMP \
+    -DTNN_OPENCL_ENABLE:BOOL=$OPENCL \
+    -DTNN_QUANTIZATION_ENABLE:BOOL=$QUANTIZATION \
+    -DTNN_BENCHMARK_MODE=ON \
+    -DTNN_BUILD_SHARED:BOOL=$SHARED_LIB 
+
+make -j7
+
+# check compile error, or ci will not stop
+# if [ 0 -ne $? ]
+# then
+#     exit -1
+# fi
